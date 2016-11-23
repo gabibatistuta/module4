4ac72052249a7650c2dba217f4f160dbd19e179d
gabibatistuta <kfragis@gmail.com>
2016-11-23 23:17:13 +0200

    telos

---
+++ script.js
@@ -0,0 +1,25 @@
+(function(){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  for (var i=0; i<names.length; i++) {
+    var firstLetter = names[i].charAt(0);
+    firstLetter = firstLetter.toLowerCase();
+
+    if (firstLetter == "j") {
+       byeSpeaker.speak(names[i]);
+    } else {
+       helloSpeaker.speak(names[i]);
+    }
+  }  
+})();
+
+
+/*Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim*/
